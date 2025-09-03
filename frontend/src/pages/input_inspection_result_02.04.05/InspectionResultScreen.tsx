a31771803f22b6e23cd0e2f3d9eb13a84b88c896
hoangpd <hoangpd@i-stech.net>
2025-09-03 17:46:49 +0700

    add first row in main screen

--- frontend/src/pages/input_inspection_result_02.04.05/InspectionResultScreen.tsx
+++ frontend/src/pages/input_inspection_result_02.04.05/InspectionResultScreen.tsx
@@ -4,6 +4,7 @@ import RightPanel from "../../component/input_inspection_result/RightPanel";
 import TopBar from "../../component/input_inspection_result/TopBar";
 import { CustomerSearchModal } from "../../component/input_inspection_result/CustomerSearchModal";
 import { useState } from "react";
+import MainBusinessScreen from "../../component/input_inspection_result/MainBusinessScreen";
 
 const InspectionResultScreen = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
@@ -11,7 +12,9 @@ const InspectionResultScreen = () => {
     <div className="w-full h-screen flex flex-col xl:text-base text-xs">
       <TopBar />
       <div className="flex-1 flex flex-row min-h-0">
-        <div className="w-full"></div>
+        <div className="w-full">
+          <MainBusinessScreen />
+        </div>
         <RightPanel />
       </div>
       {isModalOpen && (
